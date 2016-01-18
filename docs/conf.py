f174c8fbcbb8dedef1606393814b75c2386d76d8
alexandermendes <alexanderhmendes@gmail.com>
2016-01-18 02:02:51 +0000

    Add logo

--- docs/conf.py
+++ docs/conf.py
@@ -118,6 +118,7 @@ html_theme = 'flask_small'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
+    'index_logo':       'logo.png',
     'github_fork':      'alexandermendes/Flask-Z3950'
 }
 

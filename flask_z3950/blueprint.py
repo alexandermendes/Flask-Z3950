35535d54ac410cd5d962e5823de12aaee78a3024
alexandermendes <alexanderhmendes@gmail.com>
2016-01-19 14:28:22 +0000

    Add humanise int filter to blueprint

--- flask_z3950/blueprint.py
+++ flask_z3950/blueprint.py
@@ -1,6 +1,8 @@
 # -*- coding: utf8 -*-
 """Blueprint module for Flask-Z3950."""
 
+import re
+import jinja2
 from flask import Blueprint
 from .view import search
 
@@ -20,3 +22,10 @@ class Z3950Blueprint(Blueprint):
         super(Z3950Blueprint, self).__init__(**defaults)
 
         self.add_url_rule('/search/<db>', view_func=search)
+        self.add_app_template_filter(self.humanize_int)
+
+
+    @jinja2.contextfilter
+    def humanize_int(self, context, n):
+        """Add commas to a string of digits and return."""
+        return re.sub(r'(\d)(?=(\d\d\d)+(?!\d))', r'\1,', str(n))
